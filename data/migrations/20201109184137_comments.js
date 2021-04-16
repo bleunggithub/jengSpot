f97dee20b6d8eee6b8fb07a8b2d6c6678e15ca13
B Leung <leungcheukkei@gmail.com>
2021-04-17 03:36:58 +0800

    remove excessive files

--- data/migrations/20201109184137_comments.js
+++ data/migrations/20201109184137_comments.js
@@ -4,9 +4,9 @@ exports.up = function(knex) {
     .createTable('comments', function(table) {
         table.increments('id').primary();
         table.integer('users_id');
-      table.foreign('users_id').references('users.id');
-      table.string('users_userPhoto');
-      table.string('users_username')
+        table.foreign('users_id').references('users.id');
+        table.string('users_userPhoto');
+        table.string('users_username')
         table.string('commentContent');
         table.integer('posts_id');
         table.foreign('posts_id').references('posts.id');                          
--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -6,26 +6,8 @@ exports.seed = function(knex) {
       return knex('users').insert([
         {
           id: 1,
-          username: 'Betty Bee',
-          fbId: '10158412922632928',
-          userPhoto: '../assets/png/049-worldwide.png',
-          points_received: 10,
-          points_redeemed: 0,
-          admin: false
-        },
-        {
-          id: 2,
-          username: 'B L',
-          googleId: '100714625662722377353',
-          userPhoto: 'https://lh6.googleusercontent.com/-B6YTv7vT8XQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnPXc3ogaMjnsN0U6soau9Zf6X-Sg/s96-c/photo.jpg',
-          points_received: 10,
-          points_redeemed: 0,
-          admin: false
-        },
-        {
-          id: 3,
           username: 'test user',
-          password: '$2b$10$sIcFMpUpkOBi72Uwhb1P6eY2jWQrP.pM/F/.IzNsfujzEXFBmDhf2',
+          password: '$2b$10$jiYJ9x.JPzuwj4YE7T47H.Ys/mcqxiuSbSho2lXzi41c3smak0jgO',
           email: 'test@test.com',
           userPhoto: '../assets/png/049-worldwide.png',
           points_received: 0,
